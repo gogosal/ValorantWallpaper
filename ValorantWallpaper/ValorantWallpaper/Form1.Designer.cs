12fed86b448ee33c463865eba367c1e1ba0b0196
gogosal <diogo.salgueirof@gmail.com>
2024-08-19 00:31:14 +0100

    Complete base functions

--- ValorantWallpaper/ValorantWallpaper/Form1.Designer.cs
+++ ValorantWallpaper/ValorantWallpaper/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Set = new System.Windows.Forms.Button();
+            this.btn_ValorantPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Set
+            // 
+            this.btn_Set.Location = new System.Drawing.Point(143, 40);
+            this.btn_Set.Name = "btn_Set";
+            this.btn_Set.Size = new System.Drawing.Size(94, 34);
+            this.btn_Set.TabIndex = 2;
+            this.btn_Set.Text = "Set";
+            this.btn_Set.UseVisualStyleBackColor = true;
+            this.btn_Set.Click += new System.EventHandler(this.btn_Set_Click);
+            // 
+            // btn_ValorantPath
+            // 
+            this.btn_ValorantPath.Location = new System.Drawing.Point(29, 40);
+            this.btn_ValorantPath.Name = "btn_ValorantPath";
+            this.btn_ValorantPath.Size = new System.Drawing.Size(94, 34);
+            this.btn_ValorantPath.TabIndex = 0;
+            this.btn_ValorantPath.Text = "Select";
+            this.btn_ValorantPath.UseVisualStyleBackColor = true;
+            this.btn_ValorantPath.Click += new System.EventHandler(this.btn_ValorantPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(260, 126);
+            this.Controls.Add(this.btn_ValorantPath);
+            this.Controls.Add(this.btn_Set);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ValorantWallpaper";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btn_Set;
+        private System.Windows.Forms.Button btn_ValorantPath;
     }
 }
 
--- ValorantWallpaper/ValorantWallpaper/Form1.cs
+++ ValorantWallpaper/ValorantWallpaper/Form1.cs
@@ -1,20 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ValorantWallpaper
 {
     public partial class Form1 : Form
     {
+        private string _folderPath = @"C:\Riot Games\VALORANT\live\ShooterGame\Content\Movies\Menu\";
+        private string _selectedFilePath;
+        private string _latestFileName;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_ValorantPath_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Vídeos|*.mp4";
+                openFileDialog.Title = "Select the wallpaper";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _selectedFilePath = openFileDialog.FileName;
+                    MessageBox.Show($"Vídeo selecionado: {_selectedFilePath}");
+                }
+            }
+        }
+
+        private void btn_Set_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_selectedFilePath))
+            {
+                string selectedFileExtension = Path.GetExtension(_selectedFilePath).ToLower();
+                if (selectedFileExtension != ".mp4")
+                {
+                    MessageBox.Show("Please select an .mp4 file!");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(_latestFileName))
+                {
+                    _latestFileName = TakeNameOfWallpaperGame();
+                }
+
+                if (!string.IsNullOrEmpty(_latestFileName))
+                {
+                    string latestFilePath = Path.Combine(_folderPath, _latestFileName);
+
+                    try
+                    {
+                        File.Copy(_selectedFilePath, latestFilePath, overwrite: true);
+
+                        _latestFileName = Path.GetFileName(latestFilePath);
+
+                        MessageBox.Show("The wallpaper has been successfully changed!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error when changing wallpaper: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Couldn't get the file name.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No files selected.");
+            }
+        }
+
+        private string TakeNameOfWallpaperGame()
+        {
+            if (Directory.Exists(_folderPath))
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(_folderPath);
+
+                FileInfo latestFile = directoryInfo.GetFiles()
+                    .OrderByDescending(file => file.LastWriteTime)
+                    .FirstOrDefault();
+
+                if (latestFile != null)
+                {
+                    return latestFile.Name;
+                }
+                else
+                {
+                    MessageBox.Show("No files found in the folder!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("The specified directory does not exist!");
+            }
+            return null;
         }
     }
 }
